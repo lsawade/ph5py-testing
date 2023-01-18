dc9cbdc839ef1c0e9d3dd7e78bf322714df3105f
Lucas Sawade <lsawade@princeton.edu>
2023-01-18 15:27:16 -0500

    Updated the test script for a relaistic scenario

--- h5py_test.py
+++ h5py_test.py
@@ -7,8 +7,15 @@ comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 size = comm.Get_size()
 
+# Changeable
+testdir="/scratch/gpfs/lsawade"
+Nchunksizes = [1000, 1000, 100, 10]
+compressors = [None, 'gzip', 'lzf']
 
-N = np.array([2, 1, 3, 2])
+# Array size 3 x N[rank] x length, because it is an actual use case.
+N = 4500 * np.array([3, 20, 10, 3])
+length =3880
+
 Ntot = np.sum(N)
 offsets = np.hstack((np.array([0]), np.cumsum(N)))
 
@@ -17,33 +24,38 @@ if size != 4:
 
 precision = np.float16
 
-length = 5
+
 
 if rank == 0:
-    x = 0*np.ones((2, length)).astype(precision)
+    x = 0*np.ones((3, N[rank], length)).astype(precision)
 elif rank == 1:
-    x = 1*np.ones((1, length)).astype(precision)
+    x = 1*np.ones((3, N[rank], length)).astype(precision)
 elif rank == 2:
-    x = 2*np.ones((3, length)).astype(precision)
+    x = 2*np.ones((3, N[rank], length)).astype(precision)
 elif rank == 3:
-    x = 3*np.ones((2, length)).astype(precision)
+    x = 3*np.ones((3, N[rank], length)).astype(precision)
 
 
 # Test all compression styles
-for comp in [None, 'gzip', 'lzf']:
+for comp in compressors:
     
     if comp is None:
         label = 'nocomp'
     else:
         label = comp
+
         
-    with h5py.File(f'parallel_test_{comp}.h5', 'w', driver='mpio', comm=comm) as db:
+    for chunk in Nchunksizes:
+        
+        print(f"Running {comp} with chunksize {chunk}")
 
-        dset = db.create_dataset(
-            'test', (Ntot, length), dtype=precision,
-            chunks=(1, length), compression=comp)
+        with h5py.File(f'{testdir}/parallel_test_{comp}_{chunk:d}.h5', 'w', driver='mpio', comm=comm) as db:
 
-        with dset.collective:
-            if offsets[rank+1]-offsets[rank] > 0:
-                print(rank, np.min(x), np.max(x))
-                dset[offsets[rank]:offsets[rank+1], :] = x
+            dset = db.create_dataset(
+                'test', (3, Ntot, length), dtype=precision, chunks=(3, chunk, length),
+                shuffle=True, compression=comp)
+
+            print("rank", rank, "shape", x.shape)
+            with dset.collective:
+                if offsets[rank+1]-offsets[rank] > 0:
+                    dset[:, offsets[rank]:offsets[rank+1], :] = x
