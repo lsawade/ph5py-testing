3e11735e5e3b6c87cb9d677388a7aa74db5e9ffe
Lucas Sawade <lsawade@princeton.edu>
2023-03-16 15:50:08 -0400

    Changed the cmake comilation

--- vars.sh
+++ vars.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-1;95;0c
+
 # Download link
 # HDF5_LINK="https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-1.12/hdf5-1.12.1/src/hdf5-1.12.1.tar.gz"
 
@@ -18,5 +18,23 @@ export PATH=${HDF5_DIR}/bin:${PATH}
 
 # Compilers
 export MPICC=mpicc
-export MPICPP=mpic++
+export MPICXX=mpic++
 export MPIF90=mpif90
+
+
+echo "Loaded"
+echo "-----------------------------------------"
+echo "ROOT_DIR:      $ROOT_DIR"
+echo "HDF5_MAINDIR:  $HDF5_MAINDIR"
+echo "HDF5_DIR:      $HDF5_DIR"
+echo "HDF5_FC:       $HDF5_FC"
+echo "HDF5_CC:       $HDF5_CC"
+echo "MPIFC_HDF5:    $MPIFC_HDF5"
+echo "HDF5_MPI:      $HDF5_MPI"
+
+# Compilers
+echo ""
+echo "MPICC:  $MPICC"
+echo "MPICPP: $MPICPP"
+echo "MPIF90: $MPIF90"
+echo "-----------------------------------------"
